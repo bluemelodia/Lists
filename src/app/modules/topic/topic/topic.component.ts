89266294196f5407bd3bf37c6e2e5696d29797e8
bluemelodia <mlh2197@columbia.edu>
2021-05-23 20:40:32 -0400

    chore: add responsive styles to the tiles.

--- src/app/modules/topic/topic/topic.component.ts
+++ src/app/modules/topic/topic/topic.component.ts
@@ -14,7 +14,6 @@ import { TopicUtils } from '../utils/topic.utils';
 })
 export class TopicComponent implements OnInit {
   @Input() set topic(topic: Topic) {
-    console.log("pass in topic: ", topic);
     this.name = topic;
     this.imagePath = TopicUtils.imageForTopic(topic);
   }
