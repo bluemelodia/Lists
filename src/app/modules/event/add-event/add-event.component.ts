7c57f16f36987688f640111e95a9c9d8b351680f
bluemelodia <mlh2197@columbia.edu>
2021-06-13 15:04:20 -0400

    chore: restore form submission disable/enable.

--- src/app/modules/event/add-event/add-event.component.ts
+++ src/app/modules/event/add-event/add-event.component.ts
@@ -1,53 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UUID } from 'angular2-uuid';
 
 import { CalendarType } from '../../../types/calendar/calendar.types';
+import { HeaderLevel } from '../../../types/header.types';
 import { SelectedDay } from '../../../types/calendar/calendar-response.types';
 import { 
-  EventOption, 
   EventActions, 
-  eventTypes, 
-  RecurrenceOption,
-  recurrenceOptions,
-  getRecurrenceOptionByFrequency,
-  EventType
 } from '../../../types/event.types';
-import { HeaderLevel } from '../../../types/header.types';
 import { ValidationService } from '../../../services/validation.service';
 
 @Component({
-  selector: 'app-add-event',
+  selector: 'app-add-birthday',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent implements OnInit {
-  eventForm: FormGroup;
-  eventType = EventType;
-  eventTypeOptions = eventTypes;
-  eventActions: EventActions = {
+  birthdayForm: FormGroup;
+  birthdayActions: EventActions = {
     call: false,
     text: false,
     gift: false
   };
-  recurrenceOptions = recurrenceOptions;
-
-  selectedEventType: EventOption;
-  selectedDate: SelectedDay;
-  selectedRecurrence: RecurrenceOption;
-  /**
-   * Shown to the user if the respective fields are disabled.
-   */
-  prefilledDate: string;
-  prefilledRecurrence: RecurrenceOption;
   headerLevel = HeaderLevel;
+  isLunar = false;
+  selectedDate: SelectedDay;
 
   public calendarType: CalendarType = CalendarType.Lunar;
   private paramsSub;
-
-  private uuid = UUID.UUID();
-  public eventTypeID = `app-select-event-${this.uuid}`;
-  public recurrenceID = `app-select-recurrence-${this.uuid}`;
 
   constructor( 
     private fb: FormBuilder,
@@ -56,20 +35,11 @@ export class AddEventComponent implements OnInit {
 
   ngOnInit(): void {
     /* Set the controls for the form. */
-    this.eventForm = this.fb.group({
+    this.birthdayForm = this.fb.group({
       name: ['', [
         Validators.required,
         Validators.minLength(5),
         this.customValidator.nameValidator()
-      ]],
-      relationship: ['', [
-        Validators.required,
-        Validators.minLength(3),
-        this.customValidator.relationshipValidator()
-      ]],
-      other: ['', [
-        Validators.minLength(3),
-        this.customValidator.descriptionValidator()
       ]]
     });
   }
@@ -78,38 +48,18 @@ export class AddEventComponent implements OnInit {
     this.paramsSub.unsubscribe();
   }
 
-  setEventSelection(eventType: EventOption): void {
-    this.selectedEventType = eventType;
-    console.log("selected: ", this.selectedEventType);
-    this.prefilledDate = null;
-    this.prefilledRecurrence = null;
-
-    if (this.selectedEventType.month && this.selectedEventType.day) {
-      this.prefilledDate = `${this.selectedEventType.month}/${this.selectedEventType.day}`;
-
-      if (this.selectedEventType.recurrence) {
-        console.log("Set recurrence: ", this.selectedEventType.recurrence);
-        this.prefilledRecurrence = getRecurrenceOptionByFrequency(this.selectedEventType.recurrence);
-      }
-    }
-  }
-
-  setEventRecurrence(recurrence: RecurrenceOption): void {
-    this.selectedRecurrence = recurrence;
-  }
-
   setSelectedDate(selectedDate: SelectedDay): void {
     console.log("Selected date: ", selectedDate);
     this.selectedDate = selectedDate;
   }
 
   /* returns the form controls of the form. */
-  get eventFormControl() {
-    return this.eventForm.controls;
+  get birthdayFormControl() {
+    return this.birthdayForm.controls;
   }
 
   onSubmit(): void {
-    if (this.eventForm.valid) {
+    if (this.birthdayForm.valid) {
       // this.spinner.show(Spinner.RSVP, spinnerConfig);
       // this.rsvpService.findInvite(this.invitationForm.value.name);
     }
