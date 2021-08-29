4ff3f8f33a3e92dd88766aa55d8531dc1bbcaef6
bluemelodia <mlh2197@columbia.edu>
2021-08-29 14:54:19 -0400

    chore: neatly style the countries list.

--- src/app/modules/form/phone/constants/phone.constants.ts
+++ src/app/modules/form/phone/constants/phone.constants.ts
@@ -1,5 +1,11 @@
 import { CountryISO, SearchCountryField } from 'ngx-intl-tel-input';
 
-export const PREFERRED_COUNTRIES = [ CountryISO.UnitedStates ];
-export const SEARCH_COUNTRY_FIELDS = [SearchCountryField.Iso2, SearchCountryField.Name];
+export const PREFERRED_COUNTRIES = [
+	CountryISO.UnitedStates, 
+	CountryISO.Canada,
+];
+export const SEARCH_COUNTRY_FIELDS = [
+	SearchCountryField.Iso2,
+	SearchCountryField.Name,
+];
 export const STARTING_COUNTRY = CountryISO.UnitedStates;
