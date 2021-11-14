abdfcdaf3b6362f086adf3a443333d08a178a69e
Melanie H <mlh2197@columbia.edu>
2021-11-14 14:33:07 -0500

    chore: start streamlining meetings UX.

--- src/app/utils/meeting.utils.ts
+++ src/app/utils/meeting.utils.ts
@@ -212,29 +212,29 @@ export class MeetingUtils {
 	public static getMeetingMonth(month: number): string {
 		switch (month) {
 			case 0:
-				return "January";
+				return "Jan";
 			case 1:
-				return "February";
+				return "Feb";
 			case 2:
-				return "March";
+				return "Mar";
 			case 3:
-				return "April";
+				return "Apr";
 			case 4:
 				return "May";
 			case 5:
-				return "June";
+				return "Jun";
 			case 6:
-				return "July";
+				return "Jul";
 			case 7:
-				return "August";
+				return "Aug";
 			case 8:
-				return "September";
+				return "Sep";
 			case 9:
-				return "October";
+				return "Oct";
 			case 10:
-				return "November";
+				return "Nov";
 			case 11:
-				return "December";
+				return "Dec";
 		}
 	}
 
