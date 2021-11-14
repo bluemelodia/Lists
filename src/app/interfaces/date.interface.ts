6ceef50adb6f4db86c91413f9e7d54d23fb011aa
Melanie H <mlh2197@columbia.edu>
2021-11-13 19:50:14 -0500

    fix: give datestatus enums non-numerical values.

--- src/app/interfaces/date.interface.ts
+++ src/app/interfaces/date.interface.ts
@@ -1,7 +1,7 @@
 export enum DateStatus {
-	Passed,
-	ComingUp,
-	ThisWeek,
-	Tomorrow,
-	Today,
+	Passed = "Passed",
+	ComingUp = "Coming up",
+	ThisWeek = "This week",
+	Tomorrow = "Tomorrow",
+	Today = "Today",
 }
