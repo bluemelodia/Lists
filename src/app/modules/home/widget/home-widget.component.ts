4087e36f5c239201122d155e02cbd3dfba440107
Melanie H <mlh2197@columbia.edu>
2022-02-06 14:44:33 -0500

    chore: display upcoming meetings and no results.

--- src/app/modules/home/widget/home-widget.component.ts
+++ src/app/modules/home/widget/home-widget.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 
 import { Icon } from "../../../constants/icons.constants";
 import { ListType } from "../../../constants/list.constants";
+import { Topic } from "../../../constants/topics.constants";
 
-import { AddMeeting } from "../../../interfaces/service/service-objects.interface";
-import { RecipientList } from "../../../interfaces/event/recipient.interface";
+import { AddMeeting, AddRecipient } from "../../../interfaces/service/service-objects.interface";
 import { Task } from "../../../interfaces/event/task.interface";
 import { HeaderLevel } from "../../../interfaces/header.interface";
 
 import { CalendarService } from "../../../services/calendar.service";
+import { NavService } from "../../../services/nav.service";
 
 @Component({
 	selector: "app-home-widget",
@@ -18,28 +19,10 @@ import { CalendarService } from "../../../services/calendar.service";
 })
 export class HomeWidgetComponent {
 
-	@Input() list: RecipientList | AddMeeting[] | Task[];
+	@Input() list: AddRecipient[] | AddMeeting[] | Task[];
 	@Input() set type(listType: ListType) {
 		this.displayList = listType;
-
-		switch(listType) {
-			case ListType.SolarBirthdays:
-				this.title = 'Upcoming Solar Birthdays';
-				this.ariaLabel = 'Navigate to Birthdays';
-				break;
-			case ListType.LunarBirthdays:
-				this.title = 'Upcoming Lunar Birthdays';
-				this.ariaLabel = 'Navigate to Birthdays';
-				break;
-			case ListType.Tasks:
-				this.title = 'Upcoming Tasks';
-				this.ariaLabel = 'Navigate to Tasks';
-				break;
-			case ListType.Gifts:
-				this.title = 'Upcoming Gifts';
-				this.ariaLabel = 'Navigate to Gifts';
-				break;
-		}
+		this.configureWidget(listType);
 	}
 	public displayList: ListType;
 
@@ -50,14 +33,52 @@ export class HomeWidgetComponent {
 
 	public title = '';
 	public ariaLabel = '';
+	public noResultsMsg = '';
 
 	constructor(
 		private calendarService: CalendarService,
+		private navService: NavService,
 	) {
 		this.currentYear = this.calendarService.year;
 	}
 
 	public navigateToPage(listType: ListType): void {
-		
+		switch (listType) {
+			case ListType.SolarBirthdays:
+			case ListType.LunarBirthdays:
+				this.navService.navigateToTopic(Topic.Birthdays);
+				break;
+			case ListType.Tasks:
+				this.navService.navigateToTopic(Topic.Tasks);
+				break;
+			case ListType.Meetings:
+				this.navService.navigateToTopic(Topic.Meetings);
+				break;
+		}
+	}
+
+	private configureWidget(listType: ListType): void {
+		switch (listType) {
+			case ListType.SolarBirthdays:
+				this.title = 'Upcoming Solar Birthdays';
+				this.ariaLabel = 'Navigate to Birthdays';
+				this.noResultsMsg = 'No upcoming solar birthdays.';
+				break;
+			case ListType.LunarBirthdays:
+				this.title = 'Upcoming Lunar Birthdays';
+				this.ariaLabel = 'Navigate to Birthdays';
+				this.noResultsMsg = 'No upcoming lunar birthdays.';
+				break;
+			case ListType.Tasks:
+				this.title = 'Upcoming Tasks';
+				this.ariaLabel = 'Navigate to Tasks';
+				this.noResultsMsg = 'No upcoming tasks.';
+				break;
+			case ListType.Meetings:
+				this.title = 'Upcoming Meetings';
+				this.ariaLabel = 'Navigate to Meetings';
+				this.noResultsMsg = 'No upcoming meetings.';
+				break;
+		}
 	}
 }
--- src/app/pipes/meeting-formatter.pipe.ts
+++ src/app/pipes/meeting-formatter.pipe.ts
@@ -6,21 +6,31 @@ import { TimeUtils } from '../utils/time.utils';
 	name: "meetingFormatter"
 })
 export class MeetingFormatterPipe implements PipeTransform {
-	public transform(value: AddMeeting, isStart: boolean): string {
+	public transform(value: AddMeeting, isStart: boolean, shorthand: boolean): string {
 		if (value) {
 			if (isStart) {
 				const startDate = new Date(value.start_year, value.start_month - 1, value.start_date, value.start_hour, value.start_minute);
+				const startTime = TimeUtils.get12HourTime(value.start_hour, value.start_minute);
+				
+				if (shorthand) {
+					return `${value.start_month}/${value.start_date}/${value.start_year.toString().slice(-2)} ${startTime}`;
+				}
+
 				let startDateFormatted = `${TimeUtils.getDayOfWeek(startDate.getDay())} - ${TimeUtils.getMonth(startDate.getMonth())} ${TimeUtils.getDateOfMonth(startDate.getDate())}`;
 				startDateFormatted += `, ${startDate.getFullYear()}`;
 
-				const startTime = TimeUtils.get12HourTime(value.start_hour, value.start_minute);
 				return `${startDateFormatted} ${startTime}`;
 			} else {
 				const endDate = new Date(value.end_year, value.end_month - 1, value.end_date, value.end_hour, value.end_minute);
+				const endTime = TimeUtils.get12HourTime(value.end_hour, value.end_minute);
+
+				if (shorthand) {
+					return `${value.end_month}/${value.end_date}/${value.end_year.toString().slice(-2)} ${endTime}`;
+				}
+				
 				let endDateFormatted = `${TimeUtils.getDayOfWeek(endDate.getDay())} - ${TimeUtils.getMonth(endDate.getMonth())} ${TimeUtils.getDateOfMonth(endDate.getDate())}`;
 				endDateFormatted += `, ${endDate.getFullYear()}`;
 
-				const endTime = TimeUtils.get12HourTime(value.end_hour, value.end_minute);
 				return `${endDateFormatted} ${endTime}`;
 			}
 		}
